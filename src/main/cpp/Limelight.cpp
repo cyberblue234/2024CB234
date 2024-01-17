b7e6e29f3876f7962cbd4bf0a588849608eb1276
CyberBlue234 <cyberblue234@gmail.com>
2024-01-16 19:58:47 -0500

    Initial Limelight Stuff

---
+++ src/main/cpp/Limelight.cpp
@@ -0,0 +1,40 @@
+
+#include "RobotExt.h"
+#include "Limelight.h"
+
+
+void Limelight::UpdateLimelightTracking()
+{
+    std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+    target_x = table->GetNumber("tx", 0.0);
+    target_y = table->GetNumber("ty", 0.0);
+    target_area = table->GetNumber("ta", 0.0);
+    target_skew = table->GetNumber("ts", 0.0);
+    april_tag_id = table->GetNumber("tid", 0);
+
+    bot_pose = table->GetNumberArray("botpose", std::vector<double>(6));
+
+
+
+    frc::SmartDashboard::PutNumber("TX", target_x);
+    frc::SmartDashboard::PutNumber("TY", target_y);
+    frc::SmartDashboard::PutNumber("TA", target_area);
+    frc::SmartDashboard::PutNumber("APRIL_TAG_ID", april_tag_id);
+}
+
+void Limelight::SetPipelineID(PipelineID pid)
+{
+    nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("pipeline", int(pid));
+}
+
+void Limelight::SetLEDMode(LEDMode m)
+{
+    int led_mode = m;
+    nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("ledMode", led_mode);
+}
+
+void Limelight::SetCamMode(CamMode m)
+{
+    int cam_mode = m;
+    nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("camMode", cam_mode);
+}
---
+++ src/main/include/Limelight.h
@@ -0,0 +1,54 @@
+#ifndef _LIMELIGHT_H
+#define _LIMELIGHT_H
+
+#include "frc/smartdashboard/Smartdashboard.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableValue.h"
+#include "RobotMap.h"
+
+
+class Limelight
+{
+public:
+    enum LEDMode
+    {
+        kLEDPipline,
+        kLEDOff,
+        kLEDBlink,
+        kLEDOn
+    };
+    enum CamMode
+    {
+        kVisonProcessor,
+        kDriverCamera
+    };
+    enum PipelineID
+    {
+        kAprilTag,
+        kReflectiveTape
+    };
+
+
+    void UpdateLimelightTracking();
+    double GetTargetX() { return target_x; };
+    double GetTargetY() { return target_y; };
+    double GetTargetArea() { return target_area; };
+    double GetAprilTagID() { return april_tag_id; };
+    void SetLEDMode(LEDMode);
+    void SetCamMode(CamMode);
+    void SetPipelineID(PipelineID);
+
+private:
+    double target_x;
+    double target_y;
+    double target_area;
+    double target_skew;
+    double pipeline_id;
+    double april_tag_id;
+
+    std::vector<double> bot_pose;
+};
+
+#endif
