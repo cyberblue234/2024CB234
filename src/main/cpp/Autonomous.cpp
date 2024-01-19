3d42d9abd9fe4b1b8c2c13b1452d51b5d5ce5bbc
Elliott Bunnell <96508853+ejbunnell@users.noreply.github.com>
2024-01-19 08:45:58 -0500

    Possibly works?

--- src/main/cpp/Autonomous.cpp
+++ src/main/cpp/Autonomous.cpp
@@ -1,8 +1,10 @@
 #include "Autonomous.h"
+#include <frc2/command/CommandPtr.h>
 
 void Autonomous::RunAuto(std::string pathName) 
 {
     auto path = pathplanner::PathPlannerPath::fromPathFile(pathName);
 
-    pathplanner::AutoBuilder::followPath(path);
+    std::unique_ptr<frc2::Command> cmd = pathplanner::AutoBuilder::followPath(path).Unwrap();
+    cmd.Schedule();
 }
