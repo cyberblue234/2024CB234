36b28ebdedd4a57ba4b357a1122edb869803f9a9
CyberBlue234 <CyberBlue234@gmail.com>
2024-03-05 07:12:04 -0500

    log data for auto

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -94,14 +94,9 @@ frc2::CommandPtr RobotContainer::GetIntakeCommand()
 	);
 }
 
-void RobotContainer::RunTeleop()
-{
-	LogTeleopData();
-}
-
 void RobotContainer::LogTeleopData()
 {
-#define MAX_COUNT 10000
+	#define MAX_COUNT 10000
 
 	static long count = 0;
 
@@ -114,6 +109,43 @@ void RobotContainer::LogTeleopData()
 	if (count == 0)
 	{
 		t_output = fopen("/cb234/teleopdata.csv", "w");
+		logTimer.Start();
+		logTimer.Reset();
+	}
+	if (count < MAX_COUNT)
+	{
+		if (t_output != NULL)
+		{
+			if (count == 0)
+			{
+				fprintf(t_output, "Time,Volts,Shooter1RPM,Shooter2RPM\r\n");
+			}
+			fprintf(t_output, "%10.5f,%7.3f,%7.0f,%7.0f\r\n", time, volts, shooter1RPM, shooter2RPM);
+		}
+	}
+	if (t_output != NULL && count == MAX_COUNT)
+	{
+		fflush(t_output);
+	}
+	count++;
+	frc::SmartDashboard::PutNumber("LOG COUNT", count);
+}
+
+void RobotContainer::LogAutoData()
+{
+	#define MAX_COUNT 10000
+
+	static long count = 0;
+
+	double time = (double)logTimer.Get();
+	double volts = pdp.GetVoltage();
+
+	double shooter1RPM = shooter.GetShooter1RPM();
+	double shooter2RPM = shooter.GetShooter2RPM();
+
+	if (count == 0)
+	{
+		t_output = fopen("/cb234/autodata.csv", "w");
 		logTimer.Start();
 		logTimer.Reset();
 	}
