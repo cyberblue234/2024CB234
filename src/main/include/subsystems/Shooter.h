ca13e93b6672cb008cb114d58b8b4d313e999da8
CyberBlue234 <CyberBlue234@gmail.com>
2024-03-11 19:56:35 -0400

    changed 1 rpm by 2000

--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -33,7 +33,7 @@ public:
     void SetShooterMotor2(double power) { shooter2Motor.Set(power); };
     void SetShooterMotorsRPM(double rpm)
     {
-        SetShooterMotor1RPM(rpm);
+        SetShooterMotor1RPM(rpm + 2000);
         SetShooterMotor2RPM(rpm);
     };
     void SetShooterMotor1RPM(double rpm) { shooter1PID.SetReference(rpm, rev::CANSparkLowLevel::ControlType::kVelocity); };
